b76dd5c1d41e2f8deb91260b89f483a6f7d37712
Hello-one-afk <110000203+Hello-one-afk@users.noreply.github.com>
2024-05-19 15:56:35 -0400

    Creacion de ForoUsuarioLogeado con multiples componentes y estilos de Tailwind CSS
    -Se creo la vista "ForoUsuarioLogeado.vue".
    -Implementacion de los componentes 'NavbarComponente', 'AsideComponente', 'FooterComponente', 'CrearPublicacion', y 'ArticuloComponente' en 'ForoUsuarioLogeado.vue'.
    -Aplicados estilos utilizando Tailwind CSS Para mantener la coherencia con el diseño del proyecto.
    -Asegurada la correcta integración de todos los componentes en la nueva vista.

---
+++ src/views/ForoUsuarioLogeado.vue
@@ -0,0 +1,93 @@
+<template>
+
+    <header>
+        <Navbar class="w-screen"> </Navbar>
+    </header>
+
+
+    <!-- DATOS USUARIO LOGEADO
+
+    <div class="container mx-auto">
+
+        <p>Usuario: {{ nombre }}</p>
+        <p>Email: {{ correo }}</p>
+        <p>Contraseña: {{ contraseña }}</p>
+    </div>
+ -->
+
+
+
+    <div class=" flex flex-row bg-gray-100 w-screen" >
+        <aside-component class=""></aside-component>
+
+
+        <div class="flex flex-col px-20 w-screen">
+            <crear-publicacion class=""></crear-publicacion>
+            <articulo-component  ref="ArticuloComponent" ></articulo-component>
+
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+    <footer-component >
+
+    </footer-component>
+
+</template>
+
+<script>
+import Navbar from '@/components/NavbarComponent.vue'
+import AsideComponent from '@/components/AsideComponente.vue'
+import FooterComponent from '@/components/FooterComponent.vue'
+import CrearPublicacion from '@/components/CrearPublicacion.vue'
+import ArticuloComponent from '@/components/ArticuloComponente.vue'
+export default {
+    created() {
+        const usuario = JSON.parse(localStorage.getItem('usuarioLogeado'));
+        if (usuario) {
+            this.nombre = usuario.name;
+            this.correo = usuario.email;
+            this.contraseña = usuario.password;
+
+        }
+
+    },
+
+    components: {
+        Navbar,
+        AsideComponent,
+        FooterComponent,
+        ArticuloComponent,
+        CrearPublicacion,
+
+    },
+    data() {
+        return {
+            nombre: '',
+            correo: '',
+            contraseña: '',
+
+        }
+    },
+    provide(){
+        return{
+            refrescarPublicaciones: this.refrescarPublicaciones
+        }
+    },
+    methods:{
+        async refrescarPublicaciones(){
+            //accedo al componente articuloComponent al metodo fetchPublicacion con el metodo $refs
+            this.$refs.ArticuloComponent.fetchPublicaciones();
+        }
+    }
+}
+</script>
+
+<style></style>
