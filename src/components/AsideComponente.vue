dcc3d9af4ceb192bb303d231bef95257c05dbe0a
Hello-one-afk <110000203+Hello-one-afk@users.noreply.github.com>
2024-05-22 13:44:34 -0400

    Actualización de AsideComponente con verificación de usuario y cambio de imágenes en data/db.json
    - Actualizado 'AsideComponente.vue' para verificar si el usuario está logeado y mostrar nuevas funcionalidades en consecuencia y sino esta logeado igual con sus respectivas funcionalidades.
    - Actualizado 'data/db.json' para reemplazar los enlaces de imágenes de formato PNG a JPG.

--- src/components/AsideComponente.vue
+++ src/components/AsideComponente.vue
@@ -1,10 +1,10 @@
 <template>
 
 
-  <div
+  <div 
     class="flex flex-col min-h-screen sm:w-2/12 md:w-3/12 lg:w-2/12 xl:w-2/12 justify-between items-center bg-slate-200">
 
-    <div class="fixed flex flex-col bg-white rounded-xl py-4 px-4 my-4 items-start shadow   lg:w-40  xl:w-48 ">
+    <div v-if="usuario" class="fixed flex flex-col bg-white rounded-xl py-4 px-4 my-4 items-start shadow   lg:w-40  xl:w-48 ">
      
         <router-link to="/Foro" class="relative my-1">
 
@@ -20,8 +20,6 @@
         </div>
 
         </router-link> 
-      
-
         <router-link to="/Popular" class="relative my-1">
           <div class="flex justify-center px-2 py-1 text-base text-black text-center hover:bg-gray-300 hover:text-gray-900 hover:font-bold  rounded-lg
           md:px-8  lg:px-5 xl:px-3 xl:text-lg md:text-sm">
@@ -60,15 +58,42 @@
         </div>
 
         </router-link>
-    
-
-
-    
-   
-      
-
 
     </div>
+
+    <div v-if="!usuario" class="fixed flex flex-col bg-white rounded-xl py-4 px-4 my-4 items-start shadow   lg:w-40  xl:w-48 ">
+     
+     <router-link to="/ForoDefault" class="relative my-1">
+
+       <div class="flex justify-center px-2 py-1 text-base text-black text-center hover:bg-gray-300 hover:text-gray-900 hover:font-bold  rounded-lg
+       md:px-8  lg:px-5 xl:px-3 xl:text-lg md:text-sm">
+
+       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 mx-1">
+         <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
+         <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
+       </svg>
+
+       <span>Principal</span>
+     </div>
+
+     </router-link> 
+   
+
+     <router-link to="/Popular" class="relative my-1">
+       <div class="flex justify-center px-2 py-1 text-base text-black text-center hover:bg-gray-300 hover:text-gray-900 hover:font-bold  rounded-lg
+       md:px-8  lg:px-5 xl:px-3 xl:text-lg md:text-sm">
+
+       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 mx-1">
+         <path fill-rule="evenodd"
+           d="M15.22 6.268a.75.75 0 0 1 .968-.431l5.942 2.28a.75.75 0 0 1 .431.97l-2.28 5.94a.75.75 0 1 1-1.4-.537l1.63-4.251-1.086.484a11.2 11.2 0 0 0-5.45 5.173.75.75 0 0 1-1.199.19L9 12.312l-6.22 6.22a.75.75 0 0 1-1.06-1.061l6.75-6.75a.75.75 0 0 1 1.06 0l3.606 3.606a12.695 12.695 0 0 1 5.68-4.974l1.086-.483-4.251-1.632a.75.75 0 0 1-.432-.97Z"
+           clip-rule="evenodd" />
+       </svg>
+       <span class="">Popular</span>
+     </div>
+
+     </router-link>
+    
+ </div>  
 
   </div>
 
@@ -82,6 +107,20 @@
 <script>
 export default {
 
+  data() {
+    return {
+      usuario: '',
+    };
+  },
+
+  created() {
+
+    const usuario = localStorage.getItem('usuarioLogeado');
+    //si usuario contiene algo se add el valor a la variable this.usuario del componente
+    if (usuario) {
+      this.usuario = JSON.parse(usuario);
+    }
+  },
 
   methods:{
     cerrarSesionUsuario() {
