b2515fdb875608ceec6e3c894f192f8aa8f1077c
Felix216 <216felix.santana@gmail.com>
2024-05-21 21:21:35 -0400

    adicion de fuente
    se agrego la fuente inter en la configuracion de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,10 @@
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      inter: ['Inter'],
+    },
     extend: {},
   },
   plugins: [],
 }
-
