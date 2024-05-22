57da0d47f7749b6de967381335227084ee4513d0
Hello-one-afk <110000203+Hello-one-afk@users.noreply.github.com>
2024-05-22 04:32:53 -0400

    Actualización de ArticuloComponente.vue para reutilización con props
    - Actualizado el componente 'ArticuloComponente.vue' para reutilizar el código tanto para las publicaciones por defecto como para las publicaciones por popularidad.
    - Refactorizado el código para hacer uso de las props.
    - Aplicados estilos de Tailwind CSS para mantener la coherencia visual.
    - Realizadas pruebas para asegurar que el componente se comporta correctamente con props.

--- src/components/ArticuloComponente.vue
+++ src/components/ArticuloComponente.vue
@@ -1,31 +1,117 @@
 <template>
 
+  <div>
+    <div v-if="mostrar && publicacionesOrdenadas.length > 0">
 
-  <article class="overflow-hidden rounded-lg shadow transition hover:shadow-2xl my-4"
-    v-for="(publicacion, index) in publicaciones" :key="index">
-    <div class="flex-auto justify-center my-4">
-      {{ publicacion.categoria }}
-    </div>
-    <img alt="" :src="publicacion.imagen" class="h-56 w-full object-cover" />
-
-    <div class="bg-white p-4 sm:p-6">
-    
-      <h3 class="mt-0.5 text-lg text-gray-900">{{ publicacion.titulo }}</h3>
-
-      <p class="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">{{ publicacion.descripcion }}</p>
-      
-      <button v-on:click="darMeGusta()">{{ publicacion.meGusta }} </button>
-      <button v-on:click="darNoMeGusta()">{{ publicacion.noMeGusta }} </button>
-      <button v-on:click="agregarComentario()">{{ publicacion.comentarios }}</button>
-      
-
-      <div>
-
-        <router-link :to="{name:'verPublicacion', params:{publicaciones: JSON.stringify(publicacion)} }">Ver mas</router-link>
+    <article 
+      class="overflow-hidden rounded-lg shadow transition hover:shadow-2xl px-6 my-4 bg-white "
+      v-for="(publicacion, index) in publicacionesOrdenadas" :key="index">
+      <div class="flex-auto justify-center my-4">
+        {{ capitalizarPrimeraPalabra(publicacion.category) }}
       </div>
-    </div>
- 
-  </article>
+      <img alt="" :src="publicacion.image" class="h-56 w-full object-cover" />
+
+      <div class="bg-white p-4 sm:p-6">
+
+        <h3 class="mt-0.5 text-lg text-gray-900">{{ publicacion.tittle }}</h3>
+
+        <p class="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">{{ publicacion.content }}</p>
+
+        <div class="flex ">
+
+          <button v-on:click="darMeGusta()" class="flex items-center w-2/12 p-2 rounded-lg">
+            <img alt="Me gusta" src="@/assets/MeGusta.png"
+              class="mx-2 p-2 w-8 bg-gray-500 hover:bg-blue-600 rounded-lg" />
+            <span class="font-semibold text-xl">{{ publicacion.likes }}</span>
+          </button>
+
+          <button v-on:click="darNoMeGusta()" class="flex items-center w-2/12 p-2 rounded-lg">
+            <img alt="Me gusta" src="@/assets/NoMeGusta.png"
+              class="mx-2 p-2 w-8  bg-gray-500 hover:bg-blue-600 rounded-lg" />
+            <span class="font-semibold text-xl">{{ publicacion.dislikes }}</span>
+          </button>
+          <button v-on:click="agregarComentario()" class="flex items-center w-1/4 p-2 rounded-lg">
+            <img alt="Me gusta" src="@/assets/Comentario.png"
+              class="mx-2 p-2 w-8 bg-gray-500 hover:bg-blue-600  rounded-lg" />
+            <span class="font-semibold text-xl">{{ publicacion.comments }}</span>
+          </button>
+
+
+        </div>
+
+
+
+
+        <div>
+
+          <router-link :to="{ name: 'verPublicacion', params: { publicaciones: JSON.stringify(publicacion) } }">Ver
+            mas</router-link>
+        </div>
+      </div>
+
+
+
+
+    </article>
+  </div>
+
+
+    <article v-else class="overflow-hidden rounded-lg shadow transition hover:shadow-2xl px-6 my-4  bg-white "
+      v-for="(publicacion, index) in publicaciones" :key="index">
+      <div class="flex-auto justify-center my-4">
+        {{ capitalizarPrimeraPalabra(publicacion.category) }}
+      </div>
+      <img alt="" :src="publicacion.image" class="h-56 w-full object-cover" />
+
+      <div class="bg-white p-4 sm:p-6">
+
+        <h3 class="mt-0.5 text-lg text-gray-900">{{ publicacion.tittle }}</h3>
+
+        <p class="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">{{ publicacion.content }}</p>
+
+        <div class="flex ">
+
+          <button v-on:click="darMeGusta()" class="flex items-center w-2/12 p-2 rounded-lg">
+            <img alt="Me gusta" src="@/assets/MeGusta.png"
+              class="mx-2 p-2 w-8 bg-gray-500 hover:bg-blue-600 rounded-lg" />
+            <span class="font-semibold text-xl">{{ publicacion.likes }}</span>
+          </button>
+
+          <button v-on:click="darNoMeGusta()" class="flex items-center w-2/12 p-2 rounded-lg">
+            <img alt="Me gusta" src="@/assets/NoMeGusta.png"
+              class="mx-2 p-2 w-8  bg-gray-500 hover:bg-blue-600 rounded-lg" />
+            <span class="font-semibold text-xl">{{ publicacion.dislikes }}</span>
+          </button>
+          <button v-on:click="agregarComentario()" class="flex items-center w-1/4 p-2 rounded-lg">
+            <img alt="Me gusta" src="@/assets/Comentario.png"
+              class="mx-2 p-2 w-8 bg-gray-500 hover:bg-blue-600  rounded-lg" />
+            <span class="font-semibold text-xl">{{ publicacion.comments }}</span>
+          </button>
+
+
+        </div>
+
+
+
+
+        <div>
+
+          <router-link :to="{ name: 'verPublicacion', params: { publicaciones: JSON.stringify(publicacion) } }">Ver
+            mas</router-link>
+        </div>
+      </div>
+
+
+
+
+    </article>
+
+  </div>
+
+
+
+
+
 
 
 </template>
@@ -34,36 +120,61 @@
 import { obtenerPublicaciones } from '@/services/foroService';
 
 export default {
-  //debo obtener todas las publicaciones de lo usuarios y mostrarlas
   data() {
     return {
       publicaciones: [''],
     }
+
   },
- 
-  async mounted(){
-    this.fetchPublicaciones();
+  props: {
+    publicacionesOrdenadas: {
+      type: Array,
+      required: true,
+    },
+    mostrar: {
+      type: Boolean,
+      default: false,
+    },
   },
-  methods:{
-    async fetchPublicaciones(){
+
+
+
+  async mounted() {
+
+    //this.fetchPublicaciones();
+
+    if (!this.mostrar) { // Si no se deben mostrar las publicaciones ordenadas, obtener nuevas publicaciones
+      this.fetchPublicaciones();
+    }
+
+  },
+  methods: {
+    async fetchPublicaciones() {
+
       this.publicaciones = await obtenerPublicaciones();
-      console.log(this.publicaciones);
     },
-    
-   
 
-    darMeGusta(){
+    capitalizarPrimeraPalabra(string) {
+      if (!string) return '';
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    },
+
+
+
+
+    darMeGusta() {
 
     },
-    darNoMeGusta(){
+    darNoMeGusta() {
 
     },
-    agregarComentario(){
-      
+    agregarComentario() {
+
     },
 
   }
 }
+
 </script>
 
 <style></style>
