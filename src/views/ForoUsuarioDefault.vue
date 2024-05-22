9eaa535497fea96ba20da18b8df0c68e562ec6c6
Hello-one-afk <110000203+Hello-one-afk@users.noreply.github.com>
2024-05-22 07:19:17 -0400

    Creación del foro para usuarios no registrados con estilos de Tailwind CSS
    - Creada la vista 'ForoUsuariosDefault.vue' para usuarios no registrados.
    -Implentacion de varios componentes para la estructura del foro.
    - Aplicados estilos utilizando Tailwind CSS para mantener la coherencia con el diseño del proyecto.
    - Realizadas pruebas visuales para asegurar que la vista se muestra correctamente y es funcional para usuarios no registrados.

---
+++ src/views/ForoUsuarioDefault.vue
@@ -0,0 +1,51 @@
+<template>
+
+
+  <header>
+    <navbar-component>    </navbar-component>
+  </header>
+
+  <div class=" flex flex-row bg-slate-200 py-4 ">
+    <aside-componente class="flex-none w-1/3 "></aside-componente>
+
+
+    <div class="flex flex-col flex-1 py-5  bg-slate-200 ">
+      <articulo-componente class="flex-grow" ref="ArticuloComponent"> </articulo-componente>
+    </div>
+
+    <noticia-populares ref="NoticiaPopulares"> </noticia-populares>
+
+  </div>
+
+
+  <footer-component> </footer-component>
+
+</template>
+
+<script>
+import NavbarComponent from '@/components/NavbarComponent.vue';
+import AsideComponente from '@/components/AsideComponente.vue';
+import FooterComponent from '@/components/FooterComponent.vue';
+import ArticuloComponente from '@/components/ArticuloComponente.vue';
+import NoticiaPopulares from '@/components/NoticiaPopulares.vue'
+
+export default {
+  components: {
+
+    NavbarComponent,
+    AsideComponente,
+    FooterComponent,
+    ArticuloComponente,
+    NoticiaPopulares,
+  },
+  methods: {
+    async refrescarPublicaciones() {
+      //accedo al componente articuloComponent al metodo fetchPublicacion con el metodo $refs
+      this.$refs.ArticuloComponent.fetchPublicaciones();
+    }
+  }
+
+}
+</script>
+
+<style></style>
