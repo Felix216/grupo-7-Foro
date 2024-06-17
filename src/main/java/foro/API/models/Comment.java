13a41d180c45b812e7ff7d37994dcd09a820b5c8
Hello-one-afk <110000203+Hello-one-afk@users.noreply.github.com>
2024-06-16 22:54:19 -0400

    FEAT<Comment>: Añadir Entidad Comment
    -Creada la clase Comment con sus respectivos atributos y relaciones.
    -Añadidos los atributos con restricciones.

---
+++ src/main/java/foro/API/models/Comment.java
@@ -0,0 +1,46 @@
+package foro.API.models;
+
+ 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "comment")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 200, nullable = false)
+    private String content;
+
+
+
+    /*
+     * Cada comentario tendra una id_publicacion en donde estara el comentario 
+     * y un id_usuario que sera el que crea el comentario 
+     */
+
+     //Relacion con Usuario,  1 usuario puede tener muchos comentarios
+
+
+
+     //Relacion con publicaciones, 1 publicacion puede tener muchos comentarios
+     // lo mismo que decir = "Muchos comentarios estan en 1 publicacion"
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+
+
+
+}
