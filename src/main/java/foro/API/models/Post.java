f572ff8fbf764ee22f7955a2b8cc53600f93ce05
Hello-one-afk <110000203+Hello-one-afk@users.noreply.github.com>
2024-06-27 19:06:54 -0400

    feat: Actualizacion <Post> con nuevas relaciones y atributos
    -Se agrega nuevas relaciones con entidades User, Interaction y Coment
    -Se modificaron de las anteriores relaciones y atributos para mejor orientación del proyecto

--- src/main/java/foro/API/models/Post.java
+++ src/main/java/foro/API/models/Post.java
@@ -3,11 +3,17 @@ package foro.API.models;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -15,6 +21,7 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name = "post")
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,22 +31,28 @@ public class Post {
     private String tittle;
     @Column(length = 40, nullable = false)
     private String category;
-    @Column(length = 450, nullable = false)
+    @Column(length = 450 , nullable = false)
     private String content;
 
     @Column(length = 100000, nullable = false)
     private String image;
-        
-    @Column(length = 40 , nullable = false)
-    private Integer likes;
-    @Column(length = 40, nullable = false)
-    private Integer disLikes;
-   
-
-    @OneToMany(mappedBy = "post")
-    private List<Comment>comments = new ArrayList<>();
-
     
+   
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id",nullable = false)
+    @JsonIdentityReference(alwaysAsId = true)
+    private User user;
+    
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIdentityReference(alwaysAsId = true)
+    private List<Interaction> interactionsPostList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "post",cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIdentityReference(alwaysAsId = true)
+    private List<Comment> commentsPostList = new ArrayList<>();
+
+
 
 
 }
