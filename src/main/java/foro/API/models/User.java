aeda2bfb5d59e63e7f184fd05b55b70d5cf36f71
Felix216 <216felix.santana@gmail.com>
2024-06-27 19:53:21 -0400

    feat: Actualizacion de entidad <User> en carpeta models
    Se actualizo el modelo User para añadir la cardinalidad con las entidades, Comment, Post e Interaction.

--- src/main/java/foro/API/models/User.java
+++ src/main/java/foro/API/models/User.java
@@ -3,6 +3,11 @@ package foro.API.models;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,11 +17,12 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
-@Entity
 @Data
-@Table(name = "user")
+@Entity
+@Table(name = "usuarios")
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id") 
 public class User {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -39,8 +45,15 @@ public class User {
     @Column(length = 50, nullable = false)
     private String password;
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    @JsonIdentityReference(alwaysAsId = true)
+    private List<Comment> commentsListUser = new ArrayList<>();
 
-    
-    @OneToMany(mappedBy = "user")
-    private List<Comment> comments = new ArrayList<>();
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    @JsonIdentityReference(alwaysAsId = true)
+    private List<Post> postsListUser = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    @JsonIdentityReference(alwaysAsId = true)
+    private List<Interaction> interactionsListUser = new ArrayList<>();
 }
